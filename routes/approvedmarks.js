cc0822e1bae4f46f13eeda0e880994de10638ec0
piyumalanthony <piyumalanthony@gmail.com>
2018-07-07 21:57:11 +0530

    approved marks model added/ assignment_submission model commented due to error

---
+++ routes/approvedmarks.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const record = express.Router();
+
+
+// Bring in models
+let Mark=require('../models/marks');
+let ApporvedMarks=require('../models/approvedmarks');
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports=record;
